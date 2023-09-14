0417e9c00376f228ce76e29648e152a3634e7b89
Abhishek Pandey <chiseldart@hotmail.com>
2023-09-14 18:16:04 +0530

    divided banks & others

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const launchButton = document.getElementById("launch-button");
 // Add a click event listener to the "Launch Sites" button
 launchButton.addEventListener("click", () => {
     // Get the selected stock from the dropdown
-    const stockDropdown = document.querySelector(".dropdown select");
-    var selectedOption = stockDropdown.options[stockDropdown.selectedIndex].value;
+
+    var checkedBNK = document.querySelector('#banks :checked');
+    var checkedOTH = document.querySelector('#others :checked');
+    var selectedOption;
+    
+    if(checkedBNK.label !== "b n k"){
+        selectedOption = checkedBNK.value;
+    }
+    else if(checkedOTH.label !== "o t h"){
+        selectedOption = checkedOTH.value;
+    }
+    console.log(selectedOption);
 
     if (selectedOption) {
         const checkboxes = document.querySelectorAll('.checkboxes input[type="checkbox"]:checked');
@@ -13,17 +23,17 @@ launchButton.addEventListener("click", () => {
             if (checkbox.value === "TVT") {
                 // window.open(`https://www.screener.in/company//${selectedOption}`);
                 selectedOption = selectedOption === "M%26M" ? "M_M" : selectedOption;
-                window.open(`https://in.tradingview.com/symbols/NSE-`+selectedOption+`/technicals/`);
+                window.open(`https://in.tradingview.com/symbols/NSE-` + selectedOption + `/technicals/`);
             }
             if (checkbox.value === "GS") {
                 window.open(`https://www.google.com/search?q=${selectedOption.toLowerCase()}+share+price`);
             }
             if (checkbox.value === "TVS") {
                 selectedOption = selectedOption === "M%26M" ? "M_M" : selectedOption;
-                window.open(`https://in.tradingview.com/chart/Eydc2P8y/?symbol=NSE%3A`+selectedOption);
+                window.open(`https://in.tradingview.com/chart/Eydc2P8y/?symbol=NSE%3A` + selectedOption);
             }
             if (checkbox.value === "SC") {
-                window.open(`https://www.screener.in/company/`+selectedOption+`/consolidated/`);
+                window.open(`https://www.screener.in/company/` + selectedOption + `/consolidated/`);
             }
         });
     }
